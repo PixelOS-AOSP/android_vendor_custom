37f4a03ab3efc7f33199e66b8ad2601e915a83fe
Karan Parashar <whyredfire@gmail.com>
2022-08-27 13:54:24 +0530

    vendor: Import pixel_2016_exclusive XML for Google Photos
    Change-Id: I3cdb84f3e8d148b2619b5490bf18b744c5b417d4

--- config/common.mk
+++ config/common.mk
@@ -37,6 +37,10 @@ endif
 PRODUCT_COPY_FILES += \
     vendor/custom/config/permissions/privapp-permissions-lineagehw.xml:$(TARGET_COPY_OUT_SYSTEM_EXT)/etc/permissions/privapp-permissions-lineagehw.xml
 
+# Google Photos Pixel Exclusive XML
+PRODUCT_COPY_FILES += \
+    vendor/custom/prebuilt/common/etc/sysconfig/pixel_2016_exclusive.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/sysconfig/pixel_2016_exclusive.xml
+
 # Overlay
 PRODUCT_PACKAGE_OVERLAYS += \
     vendor/custom/overlay/common
