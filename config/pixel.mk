71f7b49be864a4a41a4c117e2b0cb8cda94d46b1
inferno0230 <mail@inferno0230.in>
2025-10-17 18:48:25 +0530

    [DNM] config: Stop including gsans
    breaks monet and pure black theme (?)
    
    Change-Id: I4f9df314256bde7538905eed530f81f2d52ac569
    Signed-off-by: inferno0230 <mail@inferno0230.in>

--- config/pixel.mk
+++ config/pixel.mk
@@ -7,7 +7,7 @@ $(call inherit-product, vendor/pixel/clocks/products/clocks.mk)
 $(call inherit-product, vendor/pixel/gms/products/gms.mk)
 
 # Pixel GSans
-$(call inherit-product, vendor/pixel/gsans/products/gsans.mk)
+#$(call inherit-product, vendor/pixel/gsans/products/gsans.mk)
 
 # Pixel Launcher
 $(call inherit-product, vendor/pixel/launcher/products/launcher.mk)
