ff9b77c4e6719f5a862ff23f1d59c2275a684c83
inferno0230 <mail@inferno0230.in>
2026-05-06 17:11:51 +0530

    roomservice: Use authenticated github API if token exists
    Change-Id: I3885e89e39fa5cde7caa2483c5ebee8e4ea02f94
    Signed-off-by: inferno0230 <mail@inferno0230.in>

--- build/tools/roomservice.py
+++ build/tools/roomservice.py
@@ -51,8 +51,16 @@ repositories = []
 
 if not depsonly:
     api_url = f"https://api.github.com/search/repositories?q={device}+in:name+org:PixelOS-Devices"
+    github_token = os.getenv('GITHUB_TOKEN')
+    if github_token:
+        print("Using GitHub token for authenticated requests.")
+        github_request = urllib.request.Request(api_url, headers={
+            "Authorization": "Bearer %s" % github_token
+        })
+    else:
+        github_request = api_url
     try:
-        with urllib.request.urlopen(api_url, timeout=10) as response:
+        with urllib.request.urlopen(github_request, timeout=10) as response:
             repos_json = json.loads(response.read().decode())
     except urllib.error.URLError:
         print("Failed to fetch data from GitHub API")
@@ -62,7 +70,9 @@ if not depsonly:
         sys.exit(1)
     # Extract repository names
     for repo in repos_json['items']:
+        print("Found rep: %s" % repo['name'])
         repositories.append(repo['name'])
+    sys.exit(1)
 
 local_manifests = r'.repo/local_manifests'
 if not os.path.exists(local_manifests): os.makedirs(local_manifests)
