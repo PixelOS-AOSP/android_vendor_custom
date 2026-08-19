cbf3e5df4f33c06c3053601ef1dd92c04536bbc6
AdarshGrewal <adarshgrewal@gmail.com>
2026-08-19 20:29:25 +0530

    config: Update versioning logic for release and development builds
    Leave out this custom logic for unofficial builds.
    
    Change-Id: I418881f5caeceed02dd77771bda355de3aa16389

--- config/version.mk
+++ config/version.mk
@@ -1,8 +1,16 @@
-CUSTOM_BUILD_DATE := $(shell date -u +%Y%m%d-%H%M)
-
 CUSTOM_PLATFORM_VERSION := 17.0
 
-CUSTOM_VERSION := $(CUSTOM_BUILD)-$(CUSTOM_PLATFORM_VERSION)-$(CUSTOM_BUILD_DATE)
+CUSTOM_VERSION := $(CUSTOM_BUILD)-$(CUSTOM_PLATFORM_VERSION)
+
+ifeq ($(IS_OFFICIAL),true)
+ifeq($(IS_RELEASE), true)
+CUSTOM_BUILD_DATE := $(shell date -u +%Y%m%d)
+else
+CUSTOM_BUILD_DATE := $(shell date -u +%Y%m%d-%H%M)
+endif
+CUSTOM_VERSION := $(CUSTOM_VERSION)-$(CUSTOM_BUILD_DATE)
+endif
+
 CUSTOM_VERSION_PROP := seventeen
 
 # PixelOS Platform Version
